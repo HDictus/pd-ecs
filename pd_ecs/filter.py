205d7178c3626051f2de0137b0e62506e1b4377c
hugs <h.t.dictus@gmail.com>
2022-06-19 19:42:45 +0200

    try a new approach to filters, arguably too complicated.
    Ultimately run into difficulty with setting through .loc

--- pd_ecs/filter.py
+++ pd_ecs/filter.py
@@ -5,6 +5,19 @@ import numpy as np
 import pandas as pd
 
 
+# class _MocLoc:
+
+#     def __init__(self, _filter):
+#         self._filter = _filter
+
+#     def __getitem__(self, key):
+#         return self._filter.dataframe.loc[key]
+
+#     def __setitem__(self, key, value):
+#         for c, frame in self._filter.tracked_data.items():
+#             frame.loc[key] = value
+
+
 class Filter:
     """Filter entities which have the specified components"""
 
@@ -16,6 +29,17 @@ class Filter:
         """
         self._comps = components
         self.world = world
+        self.tracked_data = {
+            comp: world[comp]
+            for comp in components}
+
+    @property
+    def loc(self):
+        return pd.core.indexing._LocIndexer('loc', self)
+
+    @property
+    def iloc(self):
+        return pd.core.indexing._iLocIndexer('iloc', self)
 
     @property
     def ids(self):
@@ -25,13 +49,30 @@ class Filter:
             ids = ids[np.isin(ids, self.world[comp].index)]
         return ids
 
+    @property
     def dataframe(self):
         """
         The data for all components in the filter,
         for the entities which have all these components
         """
         ids = self.ids
-        data = [
-            self.world[comp].loc[ids]
-            for comp in self._comps]
-        return pd.concat(data, keys=self._comps)
+        data = pd.DataFrame({
+            (comp, field): values[ids]
+            for comp, frame in self.tracked_data.items()
+            for field, values in frame.items()})
+        return data
+
+    def __getattr__(self, attr):
+        return getattr(self.dataframe, attr)
+
+    def __setitem__(self, key, value):
+        if isinstance(key, tuple):
+            self.tracked_data[key[0]].loc[self.ids, key[1:]] = value
+        else:
+            self.tracked_data[key].loc[self.ids] = value
+
+    def __getitem__(self, key):
+        return self.dataframe.loc[self.ids, key]
+
+    def __repr__(self):
+        return repr(self.dataframe)
--- tests/test_system.py
+++ tests/test_system.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from pd_ecs import System, Component, World
+import numpy as np
+import pytest as pyt
 from mock import MagicMock
+
 
 def test_system_initialized_adds_to_world():
     mockworld = MagicMock()
     sys = System(mockworld)
     assert mockworld.add_system.called_with(sys)
     assert sys.world == mockworld
+
 
 def test_system_has_filters():
     comp1 = Component('a')
@@ -27,9 +31,74 @@ def test_system_has_filters():
         filters = dict(banana=[comp1, comp2])
 
     sys = MySys(mockwld)
-    pd.testing.assert_frame_equal(
-        sys.banana,
-        pd.concat([mockwld[comp1].loc[[1, 2, 3]],
-                   mockwld[comp2].loc[[1, 2, 3]]],
-                   keys={comp1: mockwld[comp1],
-                         comp2: mockwld[comp2]}))
+    exp = pd.DataFrame({
+        (comp1, 'a'): [1, 2, 3],
+        (comp2, 'b'): [1, 2, 3]})
+    exp.index = [1, 2, 3]
+
+    pd.testing.assert_frame_equal(sys.banana[:], exp)
+
+
+def test_system_filter_modifies_world():
+    comp1 = Component('a')
+    comp2 = Component('a')
+    comp3 = Component('c')
+
+    mockwld = World(comp1, comp2, comp3)
+    mockwld.set_state({
+        comp1: pd.DataFrame({'id': [0, 1, 2, 3, 4, 5, 6, 7],
+                             'a': [0, 1, 2, 3, 4, 5, 6, 7]}).set_index('id'),
+        comp2: pd.DataFrame({'id': [1, 2, 3, 10],
+                             'a': [1, 2, 3, 10]}).set_index('id'),
+        comp3: pd.DataFrame({'id': [0, 1, 2, 4, 5, 6],
+                             'c': [1, 2, 3, 4, 5, 6]}).set_index('id')})
+
+    class MySys(System):
+
+        filters = dict(banana=[comp1, comp2])
+
+    sys = MySys(mockwld)
+
+    sys.banana[(comp1, 'a')] = 30
+    assert np.allclose(mockwld[comp1].loc[[1, 2, 3], 'a'].values, 30)
+    sys.banana[comp1] += sys.banana[comp2]
+    assert np.allclose(mockwld[comp1].loc[[1, 2, 3], 'a'],
+                       [31, 32, 33])
+    mockwld[comp1].loc[2] = 400
+    assert sys.banana.loc[2, (comp1, 'a')] == 400
+    sys.banana.loc[[2, 3]] = 44
+    assert np.allclose(mockwld[comp2].loc[[2, 3]].values, 44)
+    sys.banana.loc[[2, 3], [comp1]] = 432
+    assert np.allclose(mockwld[comp1].loc[[2, 3]].values, 432)
+    sys.banana.loc[[2, 3], [(comp1, 'a'), (comp2, 'a')]] = 110
+    assert np.allclose(mockwld[comp1].loc[[2, 3]].values, 110)
+
+    sys.banana[[comp1, comp2]] = 3000
+    assert np.allclose(mockwld[comp2].loc[[1, 2, 3]].values, 3000)
+    assert np.allclose(mockwld[comp1].loc[[1, 2, 3]].values, 3000)
+
+
+def test_system_filter_copy_breaks_stuff():
+    comp1 = Component('a')
+    comp2 = Component('a')
+    comp3 = Component('c')
+
+    mockwld = World(comp1, comp2, comp3)
+    mockwld.set_state({
+        comp1: pd.DataFrame({'id': [0, 1, 2, 3, 4, 5, 6, 7],
+                             'a': [0, 1, 2, 3, 4, 5, 6, 7]}).set_index('id'),
+        comp2: pd.DataFrame({'id': [1, 2, 3, 10],
+                             'a': [1, 2, 3, 10]}).set_index('id'),
+        comp3: pd.DataFrame({'id': [0, 1, 2, 4, 5, 6],
+                             'c': [1, 2, 3, 4, 5, 6]}).set_index('id')})
+
+    class MySys(System):
+
+        filters = dict(banana=[comp1, comp2])
+
+    sys = MySys(mockwld)
+    with pyt.raises(IndexError):
+        sys.banana.loc[4, (comp1, 'a')] = 132
+
+def test_setting_index_not_in_filter_raises_error():
+    assert False
