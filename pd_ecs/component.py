2343c84be80d1382c1f658e77f5a3a774dae8137
hugs <h.t.dictus@gmail.com>
2022-08-23 18:49:01 +0200

    Add a name to components and make them clear

--- pd_ecs/component.py
+++ pd_ecs/component.py
@@ -1,6 +1,8 @@
 """Declaring components."""
 import pandas as pd
 
+unnamed_components: list = []
+
 
 # pylint: disable=too-few-public-methods
 class Component:
@@ -9,15 +11,22 @@ class Component:
     In a component dataframe each variable is a column and each row an entity.
     """
 
-    def __init__(self, *fields):
+    def __init__(self, *fields, name=None):
         """
         Arguments:
             fields: names of component variables (str)
         """
         self.fields = fields
+        if name is None:
+            name = f'unnamed{len(unnamed_components)}'
+            unnamed_components.append(self)
+        self.name = name
 
     def init_dataframe(self):
         """
         Returns an empty dataframe with the field columns.
         """
         return pd.DataFrame(columns=self.fields, dtype=int)
+
+    def __repr__(self):
+        return f'{self.name} Component'
